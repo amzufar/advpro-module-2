79cabb76441aba49efeaca7dcde1f46a22bdd143
Ammar Zufar <amzufar@gmail.com>
2024-02-13 18:34:50 +0700

    revert changes

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,20 +1,14 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EshopApplicationTests {
 
-	@Autowired
-	private EshopApplication eshopApplication;
-
 	@Test
 	void contextLoads() {
-		assertNotNull(eshopApplication);
+		EshopApplication.main(new String[0]);
 	}
 
 }
