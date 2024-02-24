1b694d756ec70364879e881317882a50f596a356
Ammar Zufar <amzufar@gmail.com>
2024-02-24 14:04:04 +0700

    change public modifier on CarCreationService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarCreationService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarCreationService.java
@@ -3,5 +3,5 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 
 public interface CarCreationService {
-    public Car create(Car car);
+    Car create(Car car);
 }
