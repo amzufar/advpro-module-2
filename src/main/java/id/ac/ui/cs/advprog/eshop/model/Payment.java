4519ebb1923384073270386af36ed86037ca80ca
Ammar Zufar <amzufar@gmail.com>
2024-03-01 09:37:13 +0700

    Implement Payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,36 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Getter
+@Setter
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        String[] methodList = {"voucherCode", "cashOnDelivery"};
+        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
+
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+        this.paymentData = paymentData;
+    }
+
+
+}
