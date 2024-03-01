7275f2c1630825eb951fa006771f7755292be332
Ammar Zufar <amzufar@gmail.com>
2024-03-01 09:45:10 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentTest {
+
+    private Map<String, String> paymentData = (Map<String, String>) new HashMap<>().put("voucherCode", "ESHOP1234ABC5678");
+
+    @Test
+    void testCreatePayment() {
+        Payment payment = new Payment("123", "voucherCode", "SUCCESS", paymentData);
+        assertSame(this.paymentData, payment.getPaymentData());
+        assertEquals("123", payment.getId());
+        assertEquals("voucherCode", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentUsingInvalidMethod() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("123", "credit", "SUCCESS", paymentData);
+        });
+    }
+
+    @Test
+    void testSetStatusToRejected() {
+        Payment payment = new Payment("123", "voucherCode", "SUCCESS", paymentData);
+        payment.setStatus("REJECTED");
+        assertEquals("REJECTED", payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusToInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("123", "voucherCode", "MEOW", paymentData);
+        });
+    }
+}
